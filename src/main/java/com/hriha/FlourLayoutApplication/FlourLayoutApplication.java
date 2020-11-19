a0021aeb9a1c3148dd54260a3fdcc905b1ccc9b5
Mykhailo Hriha <mykhailho.hriha@gmail.com>
2020-11-19 12:22:29 +0200

    first commit

---
+++ src/main/java/com/hriha/FlourLayoutApplication/FlourLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.hriha.FlourLayoutApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlourLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlourLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hriha/FlourLayoutApplication/FlourLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hriha.FlourLayoutApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlourLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
